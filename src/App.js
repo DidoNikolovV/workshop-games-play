2596d6818b729475a135b3ba1b6b0f41a6b21998
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-09-17 14:05:07 +0300

    Add default game details page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { Header } from './components/Header/Header';
 import { Home } from './components/Home/Home';
 import { Login } from './components/Login/Login';
 import { Register } from './components/Register/Register';
+import { GameDetails } from './components/GameDetails/GameDetails';
 
 function App() {
   const [games, setGames] = useState([]);
@@ -26,7 +27,6 @@ function App() {
     <div id="box">
       <Header />
 
-      {/* Main Content */}
       <main id="main-content">
         <Routes>
           <Route path="/" element={<Home games={games} />} />
@@ -34,65 +34,10 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/create" element={<Create />} />
           <Route path="/edit" element={<Edit />} />
-          <Route path="/games" element={<Catalog games={games} />} />
+          <Route path="/catalog" element={<Catalog games={games} />} />
+          <Route path="/catalog/:gameId" element={<GameDetails />} />
         </Routes>
       </main>
-
-      {/*Details Page*/}
-      {/* <section id="game-details">
-        <h1>Game Details</h1>
-        <div className="info-section">
-          <div className="game-header">
-            <img className="game-img" src="images/MineCraft.png" />
-            <h1>Bright</h1>
-            <span className="levels">MaxLevel: 4</span>
-            <p className="type">Action, Crime, Fantasy</p>
-          </div>
-          <p className="text">
-            Set in a world where fantasy creatures live side by side with humans. A
-            human cop is forced to work with an Orc to find a weapon everyone is
-            prepared to kill for. Set in a world where fantasy creatures live side
-            by side with humans. A human cop is forced to work with an Orc to find a
-            weapon everyone is prepared to kill for.
-          </p>
-          <div className="details-comments">
-            <h2>Comments:</h2>
-            <ul>
-              <li className="comment">
-                <p>Content: I rate this one quite highly.</p>
-              </li>
-              <li className="comment">
-                <p>Content: The best game.</p>
-              </li>
-            </ul>
-            <p className="no-comment">No comments.</p>
-          </div>
-          <div className="buttons">
-            <a href="#" className="button">
-              Edit
-            </a>
-            <a href="#" className="button">
-              Delete
-            </a>
-          </div>
-        </div>
-        <article className="create-comment">
-          <label>Add new comment:</label>
-          <form className="form">
-            <textarea
-              name="comment"
-              placeholder="Comment......"
-              defaultValue={""}
-            />
-            <input
-              className="btn submit"
-              type="submit"
-              defaultValue="Add Comment"
-            />
-          </form>
-        </article>
-      </section> */}
-
     </div>
   );
 }
